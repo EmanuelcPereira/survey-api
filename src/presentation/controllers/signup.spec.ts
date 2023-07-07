8d483d80e9f044122ef0aa293827f23c05830328
Emanuel Pereira <emanuel.pereira@seidor.com.br>
2023-07-07 09:31:12 -0300

    test: ensure SignUpController returns an error if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,4 +31,20 @@ describe('SignUp Controller', () => {
       new MissingParamError('email')
     )
   })
+
+  test('should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new MissingParamError('password')
+    )
+  })
 })
