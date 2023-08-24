2e2fa9f87848446009cca8ad508d19801b6a9cf1
Emanuel Pereira <emanuel.pereira@seidor.com.br>
2023-08-24 08:52:07 -0300

    test: ensure LoginController calls Emailvalidator with valid email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,10 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helper/http-helper'
 import { type Controller, type HttpRequest, type HttpResponse } from '../../protocols'
+import { type EmailValidator } from '../signUp/signUp-protocols'
 
 export class LoginController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['email', 'password']
     for (const field of requiredFields) {
@@ -10,5 +12,6 @@ export class LoginController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+    this.emailValidator.isValid(httpRequest.body.email)
   }
 }
