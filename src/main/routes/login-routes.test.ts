cd8d46bca586d3eca37f67015dc692ca225f2cba
Emanuel Pereira <emanuel.pereira@seidor.com.br>
2023-10-26 08:53:56 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'u1cJj@example.com',
+          password: 'any_password'
+        })
+        .expect(401)
+    })
   })
 })
